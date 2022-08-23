30bdd697f8684fd8eb6a1d391889d42c332d3401
Aman singh <amanrajawat2212002@gmail.com>
2022-08-23 17:02:25 +0530

    event corrected

--- src/App.js
+++ src/App.js
@@ -3,40 +3,39 @@ import password from "./password.json";
 import "./style.css";
 import OtpInput from "react-otp-input";
 const Login = () => {
-  
   const [isSubmitted, setIsSubmitted] = useState(false);
   const [errorMessages, setErrorMessages] = useState({});
-  
+
+  // added a new state to store the value of the OTP input
+  const [code, setCode] = useState("");
+
+  // added a new state to store the current Time
+  const [currentTime] = useState(new Date().toTimeString());
+
   const errors = {
     eid: "invalid username",
     pass: "invalid password",
   };
+
   const handleSubmit = (event) => {
     //Prevent page reload
     event.preventDefault();
 
-    var { eid, pass } = document.forms[0];
+    const userData = password.find((user) => user.password === code);
+    console.log({ userData, currentTime });
 
-    // Find user login info
-    const userData = password.find((user) => user.emp_id === eid.value);
+    // add window.location.assisgn to redirect to home page
 
-    // Compare user info
     if (userData) {
-      if (userData.password !== pass.value) {
-        // Invalid password
-        setErrorMessages({ name: "pass", message: errors.pass });
-      } else {
-        setIsSubmitted(true);
-        window.location.assign("/dashboard");
-      }
+      setIsSubmitted(true);
+      // window.location.assign("/dashboard");
     } else {
-      // Username not found
-      setErrorMessages({ name: "eid", message: errors.eid });
+      alert("Invalid password"); // added an alert to show the user that the OTP is invalid
+      setCode(""); // reset the OTP input field
     }
   };
 
-
-   /// for error message
+  /// for error message
   const renderErrorMessage = (name) =>
     name === errorMessages.name && (
       <div className="error">{errorMessages.message}</div>
@@ -45,36 +44,33 @@ const Login = () => {
   const renderForm = (
     <div className="form">
       <form onSubmit={handleSubmit}>
-      <div className="input-container">
-          <label>Username </label>
-          <input type="password" name="eid" required />
-          {renderErrorMessage("eid")}
-        </div>
         <div className="input-container">
-          <label>OTP Login </label>
+          <label> LOGIN </label>
           <OtpInput
-       
-        numInputs={6}
-        separator={<span style={{ width: "8px" }}></span>}
-        isInputNum={true}
-        shouldAutoFocus={true}
-        inputStyle={{
-          border: "1px solid transparent",
-          borderRadius: "8px",
-          width: "54px",
-          height: "54px",
-          fontSize: "12px",
-          color: "#111",
-          fontWeight: "400",
-          caretColor: "blue"
-          
-        }}
-        focusStyle={{
-          border: "1px solid #CFD3DB",
-          outline: "none"
-        }}
-      />
-          </div>
+            value={code}
+            numInputs={6}
+            separator={<span style={{ width: "8px" }}></span>}
+            isInputNum={true}
+            shouldAutoFocus={true}
+            inputStyle={{
+              border: "1px solid transparent",
+              borderRadius: "8px",
+              width: "54px",
+              height: "54px",
+              fontSize: "12px",
+              color: "#171a18",
+              fontWeight: "400",
+              caretColor: "blue",
+            }}
+            focusStyle={{
+              border: "1px solid #CFD3DB",
+              outline: "none",
+            }}
+            onChange={(otp) => {
+              setCode(otp);
+            }}
+          />
+        </div>
         <div className="button-container">
           <input type="submit" />
         </div>
@@ -85,7 +81,7 @@ const Login = () => {
   return (
     <div className="app">
       <div className="login-form">
-        <div className="title">Sign In</div>
+        <div className="title">Please enter your PASSWORD to continue</div>
         {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
       </div>
     </div>
